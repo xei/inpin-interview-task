18ccb14d4902a9c789d90cfde459cd8b4b95f2fe
Hamidreza Hosseinkhani <hosseinkhani@live.com>
2019-05-03 00:30:13 +0430

    add sqlalchemy and marshmallow configs in the app.py

--- backend/app.py
+++ backend/app.py
@@ -1,3 +1,11 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:12345678@localhost/inpin_db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+ma = Marshmallow(app)
