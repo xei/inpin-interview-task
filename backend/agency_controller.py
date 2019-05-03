821e6d8d68eed655fb0141c205d279da6690a9df
Hamidreza Hosseinkhani <hosseinkhani@live.com>
2019-05-03 11:53:41 +0430

    handle probable exceptions in agency_controller.py

--- backend/agency_controller.py
+++ backend/agency_controller.py
@@ -7,35 +7,59 @@ from agency import Agency, agency_schema, agencies_schema
 def create_agency(name, parent_id):
 	'''
 	This function creates a new tupple in Agency table.
+	return 0 means everything accomplished successfully.
+	return 1 means the operation was unsuccessfull.
 	'''
-	agency = Agency(name, parent_id)
-	db.session.add(agency)
-	db.session.commit()
+	try:
+		agency = Agency(name, parent_id)
+		db.session.add(agency)
+		db.session.commit()
+		return 0
+	except Exception as e:
+		print('error while creating the agency: ' + str(e))
+		return 1
+	
 
 def read_agency(id):
 	'''
 	This function finds the first tuple with id=id in Agency table,
 	serializes it to json and returns the result.
 	'''
-	agency = Agency.query.filter_by(id=id).first()
-	result = agency_schema.dump(agency)
-	return result.data
+	try:
+		agency = Agency.query.filter_by(id=id).first()
+		result = agency_schema.dump(agency)
+		return result.data
+	except Exception as e:
+		print('error while reading agency: ' + str(e))
+		return None
 
 def read_all_agencies():
 	'''
 	This function reads, serializes and returns all the tupples in Agency table.
 	'''
-	all_agencies = Agency.query.all()
-	result = agencies_schema.dump(all_agencies)
-	return result.data
+	try:
+		all_agencies = Agency.query.all()
+		result = agencies_schema.dump(all_agencies)
+		return result.data
+	except Exception as e:
+		print('error while reading all agencies: ' + str(e))
+		return None
 
 def update_agency(id, new_name, new_parent_id):
 	'''
 	This function finds the first tupple with id=id in Agency table
 	and update it's columns.
+	return 0 means everything accomplished successfully.
+	return 1 means the operation was unsuccessfull.
 	'''
-	agency = Agency.query.filter_by(id=id).first()
-	agency.name = new_name
-	agency.parent_id = new_parent_id
-	db.session.commit()
-	return 0
+	try:
+		agency = Agency.query.filter_by(id=id).first()
+		agency.name = new_name
+		agency.parent_id = new_parent_id
+		db.session.commit()
+		return 0
+	except Exception as e:
+		print('error while updating the agency: ' + str(e))
+		return 1
+	
+	
