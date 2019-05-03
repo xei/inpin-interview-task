5862be67a6dc260aa1b27a26138a7219ff2a778b
Hamidreza Hosseinkhani <hosseinkhani@live.com>
2019-05-03 12:29:22 +0430

    add read_ad controller method

--- backend/ad_controller.py
+++ backend/ad_controller.py
@@ -1,5 +1,5 @@
 from app import db
-from ad import Ad, ad_schema, ads_schema
+from ad import Ad
 
 def create_add(agency_id, name, latitude, longitude):
 	'''
@@ -15,3 +15,23 @@ def create_add(agency_id, name, latitude, longitude):
 	except Exception as e:
 		print('error while creating the ad: ' + str(e))
 		return 1
+
+def read_ad(id):
+	'''
+	This function finds the first tuple with id=id in Ad table,
+	serializes it to json and returns the result.
+	DECIMAL type is not a serializable type,
+	so we could not use Marshmallow here and the json is made manually.
+	'''
+	try:
+		ad = Ad.query.filter_by(id=id).first()
+		ad_json = {
+			'agency_id' : ad.agency_id,
+			'name': ad.name,
+			'latitude': float(ad.latitude),
+			'longitude': float(ad.longitude)
+		}
+		return ad_json
+	except Exception as e:
+		print('error while reading the ad: ' + str(e))
+		return None
