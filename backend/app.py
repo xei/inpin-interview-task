585cdcf428dda115c71cb94ec5c21e8227eb5157
Hamidreza Hosseinkhani <hosseinkhani@live.com>
2019-05-05 22:56:03 +0430

    modify creat_db.py file to CREATE database first

--- backend/app.py
+++ backend/app.py
@@ -2,9 +2,14 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 
+DB_USER = 'root'
+DB_PASS = '12345678'
+DB_HOST = 'localhost'
+DB_NAME = 'inpin_db'
+
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:12345678@localhost/inpin_db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://{}:{}@{}/{}'.format(DB_USER, DB_PASS, DB_HOST, DB_NAME)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
--- backend/create_db.py
+++ backend/create_db.py
@@ -1,6 +1,15 @@
-from app import db
+from sqlalchemy import create_engine
+from app import DB_USER, DB_PASS, DB_HOST, DB_NAME, db
 from agency import Agency
 from ad import Ad
 
-db.drop_all()
+# Connect to database server
+db_uri = 'mysql+pymysql://{}:{}@{}'.format(DB_USER, DB_PASS, DB_HOST)
+engine = create_engine(db_uri)
+
+# Create Database
+query_create_db = 'CREATE DATABASE IF NOT EXISTS {} ;'.format(DB_NAME)
+engine.execute(query_create_db)
+
+# Create tables
 db.create_all()
