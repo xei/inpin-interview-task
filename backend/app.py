7459b83547704e8b7a6199f1812fe700734c1d5f
Hamidreza Hosseinkhani <hosseinkhani@live.com>
2019-05-12 21:37:38 +0430

    add port config to the app

--- backend/app.py
+++ backend/app.py
@@ -5,11 +5,12 @@ from flask_marshmallow import Marshmallow
 DB_USER = 'root'
 DB_PASS = '12345678'
 DB_HOST = 'db'
+DB_PORT = '3306'
 DB_NAME = 'inpin_db'
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://{}:{}@{}/{}'.format(DB_USER, DB_PASS, DB_HOST, DB_NAME)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://{}:{}@{}:{}/{}'.format(DB_USER, DB_PASS, DB_HOST, DB_PORT, DB_NAME)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
